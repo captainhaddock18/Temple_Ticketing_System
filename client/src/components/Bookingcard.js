d39cb7037786dbb13e3464b73fa4b12bad57b1fd
THARAK HEGDE <b22mt044@iitj.ac.in>
2024-03-31 22:51:58 +0530

    card

---
+++ client/src/components/Bookingcard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/header'
+
+export default function Bookingcard(props) {
+    return (
+
+        <div class="card bg-light" style={{width: 18+'rem', margin: 10 +'px'}}>
+      <img src={props.img} class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">{props.title}</h5>
+        <p class="card-text">{props.text}</p>
+        <a href="#" class="btn btn-primary">Book Now!</a>
+      </div>
+    </div>
+    
+      )
+}
+
